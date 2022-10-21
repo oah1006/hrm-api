04beb6d6fc6ca404002ac9d975786e90d4c07c6c
OaH <81689054+oah1006@users.noreply.github.com>
2022-10-21 11:26:16 +0700

    create employees table

---
+++ database/migrations/2022_10_21_041632_create_employees_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('employees', function (Blueprint $table) {
+            $table->id();
+            $table->string('first_name')->index();
+            $table->string('last_name')->index();
+            $table->string('email')->unique();
+            $table->string('phone_number')->unique();
+            $table->date('birth_date')->index();
+            $table->enum('gender', [0, 1, 2])->index();
+            $table->string('password');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('employees');
+    }
+};
