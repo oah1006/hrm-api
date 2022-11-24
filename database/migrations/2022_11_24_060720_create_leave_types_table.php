4d99ab9720774288fc79058fd868f6dc7dc87c13
OaH <81689054+oah1006@users.noreply.github.com>
2022-11-24 13:13:42 +0700

    build frame leave type

--- database/migrations/2022_11_24_060720_create_leave_types_table.php
+++ database/migrations/2022_11_24_060720_create_leave_types_table.php
@@ -13,13 +13,10 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('leave_types', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->string('type_name');
+            $table->string('leave_id');
             $table->timestamps();
         });
     }
@@ -31,6 +28,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('leave_types');
     }
 };
