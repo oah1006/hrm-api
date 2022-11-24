f7134fae6d595561f153cf02f201abce4ae09fbb
OaH <81689054+oah1006@users.noreply.github.com>
2022-11-24 12:53:37 +0700

    Build frame leave

---
+++ app/Models/Leave.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Leave extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'leave_type',
+        'start_day',
+        'end_day',
+        'reason',
+        'status'
+    ];
+}
---
+++ database/migrations/2022_11_24_051438_create_leaves_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('leaves', function (Blueprint $table) {
+            $table->id();
+            $table->string('leave_type');
+            $table->date('start_day');
+            $table->date('end_day');
+            $table->string('reason');
+            $table->enum('status', ['pending', 'approved', 'rejected']);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('leaves');
+    }
+};
